6572a0d01e5517e37899fe1832c091a16c314cc6
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-10-15 23:52:01 +0300

    Create DeepFlatten.js

---
+++ DeepFlatten.js
@@ -0,0 +1,9 @@
+// Deep flattens an array.
+
+// Array.prototype.concat() with an empty array and the spread operator to flatten an array. 
+// Recursively flatten each element that is an array.
+
+const deepFlatten = (arr) => [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
+
+Exp.
+  deepFlatten([1, [2], [[3], 4], 5]); // [1,2,3,4,5]
